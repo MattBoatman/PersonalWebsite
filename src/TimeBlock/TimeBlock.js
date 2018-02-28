aa0fd3583e0cca171fb1af90dbf99dde2a26f973
Matt Boatman <matthewd.boatman@gmail.com>
2018-02-28 14:25:01 -0500

    testing prettier commit hook

--- src/TimeBlock/TimeBlock.js
+++ src/TimeBlock/TimeBlock.js
@@ -1,15 +1,17 @@
-import React from "react";
-import PropTypes from "prop-types";
-import "./TimeBlock.css";
+import React from 'react';
+import PropTypes from 'prop-types';
+import './TimeBlock.css';
 
 const TimeBlock = props => {
   return (
     <div>
       <div className="header">
-        <span><b>{props.place}</b></span>
+        <span>
+          <b>{props.place}</b>
+        </span>
         <span className="smallerText">{props.duration}</span>
       </div>
-        <p className="title smallerText">{props.title}</p>
+      <p className="title smallerText">{props.title}</p>
     </div>
   );
 };
